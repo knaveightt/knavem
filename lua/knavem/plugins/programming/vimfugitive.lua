dc301b8c6231256a59cbc218503efe3aa01c3060
John Osinski <johnosinski80@gmail.com>
2025-02-18 20:59:46 -0500

    more git keymap updates

--- lua/knavem/plugins/programming/vimfugitive.lua
+++ lua/knavem/plugins/programming/vimfugitive.lua
@@ -8,5 +8,7 @@ return {
         { "<leader>gb", "<cmd>Git blame<CR>", desc = "Current File Blame"},
         { "<leader>gc", "<cmd>Git commit<CR>", desc = "Git Commit"},
         { "<leader>gP", "<cmd>Git push<CR>", desc = "Push Commits"},
+        { "<leader>gp", "<cmd>Git pull<CR>", desc = "Push Commits"},
+        { "<leader>gf", "<cmd>Git fetch<CR>", desc = "Push Commits"},
     },
 }
