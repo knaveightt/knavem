6b8566067035ec1251d985483c82d4e38cf9c470
John O <johnosinski80@gmail.com>
2025-02-19 20:55:11 -0500

    consolidated neovide configuration

--- lua/knavem/core/init.lua
+++ lua/knavem/core/init.lua
@@ -1,4 +1,3 @@
 require("knavem.core.options")
 require("knavem.core.keymaps")
-require("knavem.core.functions")
 require("knavem.core.neovide")
--- lua/knavem/core/neovide.lua
+++ lua/knavem/core/neovide.lua
@@ -2,3 +2,12 @@
 if vim.g.neovide then
     vim.o.guifont = "JetBrainsMono Nerd Font:h14"
 end
+
+---
+--- Neovide Custom Functions
+---
+
+vim.api.nvim_create_user_command("NeovideToggleFullscreen",
+    "let g:neovide_fullscreen = !g:neovide_fullscreen", {})
+vim.keymap.set("n", "<F11>", "<cmd>NeovideToggleFullscreen<CR>", { desc = "Neovide Toggle Fullscreen" })
+
